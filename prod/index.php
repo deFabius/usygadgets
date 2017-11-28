32843030f9e601d29793b90c3d0bf05560c1e42f
deFabius <MayDay@01>
2017-11-28 00:57:28 +0000

    build for prod

--- prod/index.php
+++ prod/index.php
@@ -1,12 +1,18 @@
 <?php get_header(); ?>
 
 <?php
+echo "<div class=\"navigation-fixed\">";
+wp_nav_menu( array( 
+    'theme_location' => 'main-menu', 
+    'menu' => 'main-menu',
+    'container' => 'div',
+    'container_class' => 'main_menu' ) );
+echo "</div>";
+
 if ( have_posts() ) {
     while ( have_posts() ) {
  
         the_post(); ?>
- 
-        <h2><?php the_title(); ?></h2>
  
         <?php the_content(); ?>
  
